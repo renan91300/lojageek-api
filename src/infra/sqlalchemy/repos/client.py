6e24cd503784d1d7a08a00384ecd8645d29d23b0
Fernanda Oliveira <82584841+fernandaorms@users.noreply.github.com>
2024-06-13 15:47:37 -0300

    Update client.py

--- src/infra/sqlalchemy/repos/client.py
+++ src/infra/sqlalchemy/repos/client.py
@@ -8,7 +8,7 @@ class RepoClient():
     def __init__(self, db: Session):
         self.db = db
         
-
+ 
 
     def create(self, client: schemas.Client):# Covertendo o Schema em um modelo
         db_client = models.Client(
@@ -32,31 +32,39 @@ class RepoClient():
 
 
     def update(self, client: schemas.Client):
-        print(client)
+        db_client = self.db.query(models.Client).filter(models.Client.id == client.id).first() is not None
 
-        update_stmt = update(models.Client).where(
-            models.Client.id == client.id
-        ).values(
-            first_name=client.first_name, 
-            last_name=client.last_name,
-            email=client.email,
-            password=client.password,
-            address=client.address,
-            phone=client.phone,
-            cpf=client.cpf
-        )
+        if db_client:
+            update_stmt = update(models.Client).where(
+                models.Client.id == client.id
+            ).values(
+                first_name=client.first_name, 
+                last_name=client.last_name,
+                email=client.email,
+                password=client.password,
+                address=client.address,
+                phone=client.phone,
+                cpf=client.cpf
+            )
 
-        self.db.execute(update_stmt)
-        self.db.commit()
+            self.db.execute(update_stmt)
+            self.db.commit()
 
-        return  client
+            return  True
+        raise ValueError('ID do cliente não encontrado.')
     
 
     def delete(self, client_id: int):
+        clients_with_order = self.db.query(models.Order).filter(models.Order.client_id == client_id).first()
+
+        if clients_with_order:
+            raise ValueError('Não é possível deletar o cliente pois há pedidos associados à ele.')
+
         db_client = self.db.query(models.Client).filter(models.Client.id == client_id).first()
 
         if db_client:
             self.db.delete(db_client)
             self.db.commit()
+            
             return True
-        return False
+        raise ValueError('ID do cliente não encontrado.')
